c061ba7fab72ebda74b79f5578ec1e873e2f8850
Гурков Леонид Олегович <gurkov_lo@rtsoft.msk.ru>
2018-03-14 15:48:08 +0300

    pf.py change

--- buses/backend/pf.py
+++ buses/backend/pf.py
@@ -1,6 +1,9 @@
 import sys
 
-sys.path.append("C:\\Program Files\\DIgSILENT\\PowerFactory 2016 SP4\\Python\\3.4")
+LIB_PATH = "C:\\Program Files\\DIgSILENT\\PowerFactory 2016 SP4\\Python\\3.4"
+PROJECT_PATH = "\gurkov_lo.IntUser\Богучанская ГЭС.IntPrj"
+
+sys.path.append(LIB_PATH)
 
 
 class PowerFactory:
@@ -12,5 +15,5 @@ class PowerFactory:
         if app is None:
             raise Exception("getting PoerFactory application failure")
 
-        app.ActivateProject("\gurkov_lo.IntUser\Богучанская ГЭС.IntPrj")
+        app.ActivateProject(PROJECT_PATH)
         return app, powerfactory
